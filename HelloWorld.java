fb4b6933683817441d3f6a09603358af598aab5f
Kiran Mahajan <mahajankiran020@gmail.com>
2022-08-23 12:19:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,6 @@
  */
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hi,Kiran Here.Hello World");
+    System.out.println("Hi,Kiran Here.How are you?");
   }
 }
